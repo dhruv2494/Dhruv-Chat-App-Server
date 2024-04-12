a47d2753bdafb412db7ac92d58663a075ada9a2e
dhruv2494 <bhuvadhruv24@gmail.com>
2024-04-12 18:39:31 +0530

    Changes_3

--- index.js
+++ index.js
@@ -39,8 +39,88 @@
 //   console.log(`Server started on port ${PORT}`);
 // });
 
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const { Server } = require("socket.io");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const userRoute = require("./routes/userRoutes");
+// require("dotenv").config();
+
+// const PORT = process.env.PORT || 4000;
+
+// const app = express();
+
+// const mongooseConnect = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URL);
+//     console.log("Connected to MongoDB!");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// mongooseConnect();
+
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use("/user", userRoute);
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome to Socket.IO server!");
+// });
+
+// // Set up Socket.io with Express
+// const server = app.listen(PORT, () => {
+//   console.log(`Server started on port ${PORT}`);
+// });
+
+// const io = new Server(server, {
+//   cors: {},
+// });
+
+// io.on("connection", (socket) => {
+//   socket.join("room");
+
+//   socket.on("new-user-joined", (Name) => {
+//     socket.broadcast.to("room").emit("user-join", Name);
+//   });
+
+//   socket.on("send-msg", (msg) => {
+//     socket.broadcast.to("room").emit("receive-msg", msg);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+// });
+
+// module.exports = app;
+
+
+
+
+
+
+
+
+
+
 const express = require("express");
 const { Server } = require("socket.io");
+const http = require("http");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -50,6 +130,11 @@ require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {},
+});
 
 const mongooseConnect = async () => {
   try {
@@ -70,24 +155,17 @@ app.get("/", (req, res) => {
   res.send("Welcome to Socket.IO server!");
 });
 
-// Set up Socket.io with Express
-const server = app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
-
-const io = new Server(server, {
-  cors: {},
-});
+io.attach(server);
 
 io.on("connection", (socket) => {
   socket.join("room");
 
   socket.on("new-user-joined", (Name) => {
-    socket.broadcast.to("room").emit("user-join", Name);
+    socket.to("room").broadcast.emit("user-join", Name);
   });
 
   socket.on("send-msg", (msg) => {
-    socket.broadcast.to("room").emit("receive-msg", msg);
+    socket.to("room").broadcast.emit("receive-msg", msg);
   });
 
   socket.on("disconnect", () => {
